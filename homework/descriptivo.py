ef40b2f7fd894773f2e4fbe0675a8ad75b9f3721
Juan Pablo Salas Clavijo <jsalasc@unal.edu.co>
2025-09-13 11:52:31 -0500

    entrega Homework 13-09

---
+++ homework/descriptivo.py
@@ -0,0 +1,25 @@
+# %%
+import pandas as pd  #  type: ignore
+
+df = pd.read_csv("../files/input/house_data.csv")
+
+features = df[
+    [
+        "bedrooms",
+        "bathrooms",
+        "sqft_living",
+        "sqft_lot",
+        "floors",
+        "waterfront",
+        "condition",
+    ]
+]
+
+target = df[["price"]]
+target
+# %%
+
+print("hola mundo")
+
+
+# %%
